25cb2537bd0eff14da0d880d043d80ce788fa944
Bikra-m <bikrampanda24589@gmail.com>
2021-11-17 11:08:00 +0530

    variable changes

--- src/main/java/in/ashokit/Application.java
+++ src/main/java/in/ashokit/Application.java
@@ -1,11 +1,15 @@
 package in.ashokit;
 
+import java.util.HashMap;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
 
+	HashMap<Integer, String> map=null;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
