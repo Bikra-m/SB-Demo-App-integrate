b88db2bb779efed3c20a5f9927e3debc05b969d2
Bikra-m <bikrampanda24589@gmail.com>
2021-11-17 11:34:10 +0530

    Story no 200

--- src/main/java/in/ashokit/Application.java
+++ src/main/java/in/ashokit/Application.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 
+	String urp_story1="https://localhost:7070/welcome";
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
